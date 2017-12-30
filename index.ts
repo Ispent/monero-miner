1506f8154cc99bf2c1baf09bf840e34c6ae31675
Ispent <34940889+Ispent@users.noreply.github.com>
2017-12-29 22:05:45 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,33 @@
+import * as execa from 'execa'
+import { dirname, resolve } from 'path'
+
+export const minerExecutablePath = resolve(__dirname, '../xmrig/build/xmrig')
+
+export function startMiner(
+  pool: string,
+  username: string,
+  password?: string,
+  additionalArgs?: string[],
+) {
+  const args = [
+    '-o',
+    pool,
+    '-u',
+    username,
+    '-k',
+    '--donate-level=1',
+    '--print-time=10',
+    ...(additionalArgs || []),
+  ]
+  if (password) {
+    args.push('-p', password)
+  }
+  return execa(minerExecutablePath, args, {
+    env: {
+      LD_LIBRARY_PATH: `${process.env.LD_LIBRARY_PATH}:${resolve(
+        dirname(minerExecutablePath),
+        'lib',
+      )}`,
+    },
+  })
+}
